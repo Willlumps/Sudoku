59f149f50ab6f3cbca962ed3e2d2964880399736
Willlumps <nathanielwiller93@gmail.com>
2020-02-24 14:25:19 -0500

    test

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,8 +1,16 @@
 import tkinter
+from tkinter import *
 import sys
 
 print (sys.version)
 
 
 gui = tkinter.Tk(className = 'Sudoku')
+#gui.geometry('500x500')
+
+
+for i in range(9):
+    for j in range(9):
+        btn = Button(gui, height = 4, width = 5)
+        btn.grid(column=j, row=i)
 gui.mainloop()
