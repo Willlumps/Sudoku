466ebabd8e3cbf87b2c3f8b4e957414b81e1f17d
Willlumps <55333944+Willlumps@users.noreply.github.com>
2020-02-29 17:28:27 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -16,6 +16,8 @@ class Board():
 
 class GUI(Frame):
     buttons = [[[] for i in range(9)] for j in range(9)]
+    currentButton = buttons[0][0]
+    buttonText = ""
 
     def __init__(self, parent, game):
         self.game = game
@@ -38,31 +40,63 @@ class GUI(Frame):
 
     def __dboard(self):
         self.pixel = tkinter.PhotoImage(width=1, height=1)
+        self.selectedButton = StringVar()
+        self.test = StringVar()
         count=0
+        print (self.game.puzzle()[1][2])
         for i in range(9):
             for j in range(9):
-                btnText = self.game.puzzle()[j][i] if self.game.puzzle()[j][i] != 0 else " "
+                btnText = self.game.puzzle()[i][j] if self.game.puzzle()[i][j] != 0 else " "
                 if btnText == " ":
-                    btn = Button(self.parent, image=self.pixel, text=btnText, width=50, height=50, compound="left", command=lambda row=j, col=i: self.action(col, row))
+                    # btn = Button(self.parent, image=self.pixel, text=btnText, width=50, height=50, compound="left", 
+                    #              command=lambda row=j, col=i: self.action(col, row))
+                    btn = Radiobutton(self.parent, image=self.pixel, text=btnText, value=count, variable=self.selectedButton, width=50, height=50, 
+                                      compound="left", indicatoron=False, command=lambda row=i, col=j: self.getRowCol(col, row))
                 else:
-                    btn = Button(self.parent, image=self.pixel, text=btnText, font=("bold"), disabledforeground="#CC6666", width=50, height=50, 
-                                 compound="left", state=DISABLED, command=lambda row=j, col=i: self.action(col, row))
-                self.buttons[j][i] = btn
-                if j == 2 or j == 5:
-                    btn.grid(column=i, row=j, pady=(0, 2))
+                    # btn = Button(self.parent, image=self.pixel, text=btnText, font=("bold"), disabledforeground="#CC6666", width=50, 
+                    #               height=50, compound="left", state=DISABLED, command=lambda row=j, col=i: self.action(col, row))
+                    btn = Radiobutton(self.parent, image=self.pixel, text=btnText, font=("bold"), value=count, variable=self.selectedButton, 
+                                      state=DISABLED, disabledforeground="#CC6666", width=50, height=50, compound="left", indicatoron=False)
+                self.buttons[i][j] = btn
                 if i == 2 or i == 5:
-                    btn.grid(column=i, row=j, padx=(0, 2))
+                    btn.grid(column=j, row=i, pady=(0, 2))
+                if j == 2 or j == 5:
+                    btn.grid(column=j, row=i, padx=(0, 2))
                 else:
-                    btn.grid(column=i, row=j)
+                    btn.grid(column=j, row=i)
                 count += 1
 
-        
-        #print(np.matrix(self.buttons))
+        one = Button(self.parent, image=self.pixel, text="1", width=50, height=50, compound="left", command=lambda : self.placeShit('1'))
+        two = Button(self.parent, image=self.pixel, text="2", width=50, height=50, compound="left", command=lambda : self.placeShit('2'))
+        three = Button(self.parent, image=self.pixel, text="3", width=50, height=50, compound="left", command=lambda : self.placeShit('3'))
+        four = Button(self.parent, image=self.pixel, text="4", width=50, height=50, compound="left", command=lambda : self.placeShit('4'))
+        five = Button(self.parent, image=self.pixel, text="5", width=50, height=50, compound="left", command=lambda : self.placeShit('5'))
+        six = Button(self.parent, image=self.pixel, text="6", width=50, height=50, compound="left", command=lambda : self.placeShit('6'))
+        seven = Button(self.parent, image=self.pixel, text="7", width=50, height=50, compound="left", command=lambda : self.placeShit('7'))
+        eight = Button(self.parent, image=self.pixel, text="8", width=50, height=50, compound="left", command=lambda : self.placeShit('8'))
+        nine = Button(self.parent, image=self.pixel, text="9", width=50, height=50, compound="left", command=lambda : self.placeShit('9'))
 
-    def action(self, row, col):
-        #button = buttons[btn]
-        #print(self.buttons)
-        self.buttons[col][row].config(text="dicks")
+        one.grid(column=0, row=9, pady=(20, 0))
+        two.grid(column=1, row=9, pady=(20, 0))
+        one.grid(column=0, row=9, pady=(20, 0))
+        two.grid(column=1, row=9, pady=(20, 0))
+        three.grid(column=2, row=9, pady=(20, 0))
+        four.grid(column=3, row=9, pady=(20, 0))
+        five.grid(column=4, row=9, pady=(20, 0))
+        six.grid(column=5, row=9, pady=(20, 0))
+        seven.grid(column=6, row=9, pady=(20, 0))
+        eight.grid(column=7, row=9, pady=(20, 0))
+        nine.grid(column=8, row=9, pady=(20, 0))
+
+    def getRowCol(self, row, col):
+        self.currentButton = self.buttons[col][row]
+        print(self.selectedButton.get())
+    def placeShit(self, tex):
+        # self.selectedButton
+        self.currentButton.config(text=tex)
+    
+
+
 
     def possiblePlay(self, row, col, num):
         for i in range(9):
